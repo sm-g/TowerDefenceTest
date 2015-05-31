7fb635f7ab960e842f54f48e1032aa399206df4f
smg <semagerasimov@gmail.com>
2015-05-31 13:17:58 +0700

    add TurretPlacer

---
+++ Assets/Scripts/TurretPlacer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TurretPlacer : Singleton<TurretPlacer>
+{
+    public Transform[] turretPrefabs;
+
+    private List<TurretAI> _turrets = new List<TurretAI>();
+
+    public IEnumerable<TurretAI> Turrets { get { return _turrets; } }
+
+    private void Awake()
+    {
+        if (turretPrefabs.Length == 0)
+            Debug.LogError("Add turret prefabs to " + typeof(SpawnerAI));
+
+        foreach (var prefab in turretPrefabs)
+        {
+            var ai = prefab.GetComponent<TurretAI>();
+            if (ai == null)
+                Debug.LogError("No " + typeof(TurretAI) + " in prefab");
+            else
+                _turrets.Add(ai);
+        }
+    }
+}
