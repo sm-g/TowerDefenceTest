47da369fc62df0fc9de7b422e96b91eae7371cb0
smg <semagerasimov@gmail.com>
2015-06-02 15:38:04 +0700

    fix Mob max HP

--- Assets/Scripts/MobHP.cs
+++ Assets/Scripts/MobHP.cs
@@ -4,35 +4,35 @@ namespace Assets.Scripts
 {
     public class MobHP : MonoBehaviour
     {
-        [Range(0, 100)]
+        [Range(0, 500)]
         public int maxHP = 30;
 
-        [Range(0, 100)]
+        [Range(0, 500)]
         public int curHP = 30;
 
-        public Color maxDamageColor = Color.red;
-        public Color minDamageColor = Color.blue;
+        public Color minHpColor = Color.red;
+        public Color maxHpColor = Color.blue;
+
+        private Material material;
 
         private void Awake()
         {
             GameManager.Instance.Register(gameObject);
             curHP = Mathf.Min(curHP, maxHP); // curHP <= maxHP
+            material = gameObject.GetComponent<Renderer>().material;
+        }
+
+        private void Update()
+        {
+            material.color = Color.Lerp(minHpColor, maxHpColor, (float)curHP / maxHP);
+            if (curHP <= 0)
+                Destroy(gameObject);
         }
 
         public void TakeDamage(int damage)
         {
             if (damage > 0)
                 curHP -= damage;
-        }
-
-        private void Update()
-        {
-            // цвет моба зависит от hp
-            gameObject.GetComponent<Renderer>().material.color = Color.Lerp(maxDamageColor, minDamageColor, curHP / maxHP);
-            if (curHP <= 0)
-            {
-                Destroy(gameObject);
-            }
         }
 
         private void OnDestroy()
